5ccc02dcfa3274ee2e5d04ac696defdb7ad2d2dd
pulin2004 <pulind@sina.com>
2016-11-25 19:57:56 +0800

    Initial commit

---
+++ src/main/java/com/pl/java/main/App.java
@@ -0,0 +1,12 @@
+package com.pl.java.main;
+
+import com.pl.scala.main.ScalaPrint;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+        ScalaPrint.print();
+    }
+}
---
+++ src/main/scala/ScalaPrint.scala
@@ -0,0 +1,9 @@
+package com.pl.scala.main
+/**
+  * Created by pulin on 16/11/25.
+  */
+object ScalaPrint {
+  def print():Unit={
+    println("I love scala");
+  }
+}
