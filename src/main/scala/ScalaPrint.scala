d345c3e6b74ddcc1b8f62c2de4a01bd0e162b567
pulin2004 <pulind@sina.com>
2016-11-25 20:12:11 +0800

    Update ScalaPrint.scala
    测试

--- src/main/scala/ScalaPrint.scala
+++ src/main/scala/ScalaPrint.scala
@@ -3,9 +3,7 @@ package com.pl.scala.main
   * Created by pulin on 16/11/25.加了
   * 第一次测试成功
   * 第二次测试
-  * 本地第三次测试
-  * 官网第三次测试
-  * 合并成功
+  * 官网第四次测试
   */
 object ScalaPrint {
   def print():Unit={
